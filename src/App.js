42bcff230434a142f21e1fe917b8a865b2b5f419
yousef-bigdeli <yousefbigdeli96@gmail.com>
2022-04-08 22:01:51 +0430

    Change global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import TodoApp from './Components/TodoApp';
 
 function App() {
   return (
     <div className="App">
       <h1>TodoList App</h1>
-      <div className="container"></div>
+      <TodoApp />
     </div>
   );
 }
